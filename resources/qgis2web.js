e392809edb69c78289702c978eb87cb2721314fd
UNHabitatAndinos <unhabitatandinos@gmail.com>
2021-01-18 14:57:11 -0500

    Visor

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -37,7 +37,7 @@ layerSwitcher.hidePanel = function() {};
 layerSwitcher.showPanel();
 
 
-map.getView().fit([-8088803.839014, 871391.893131, -8053636.178689, 890387.854902], map.getSize());
+map.getView().fit([-8085841.007835, 871385.930219, -8055872.654489, 888987.797083], map.getSize());
 
 var NO_POPUP = 0
 var ALL_FIELDS = 1
